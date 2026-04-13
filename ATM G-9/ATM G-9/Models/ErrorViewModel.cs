12e8e4f82a017438d65122fa6df0b81c1b0e0b5a
Kaledcr <kaledgrojas@gmail.com>
2026-04-13 02:30:01 -0600

    Getting started 2.0

---
+++ ATM G-9/ATM G-9/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATM_G_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
